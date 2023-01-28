e53b87a3c7768a4043c9e93645f47c4ade713273
cgauci87 <cgauci@gmail.com>
2023-01-28 10:43:42 +0000

    added the main header component for 'shop' -- src/components/shop/navbar/HeaderShop.js

--- src/components/shop/navbar/HeaderShop.js
+++ src/components/shop/navbar/HeaderShop.js
@@ -3,17 +3,17 @@ import {Link} from 'react-router-dom';
 
 import HeaderRight from './HeaderRightShop';
 import Navbar from './NavbarShop';
-
-import data from '../../data/topbar-text.json';
+import data from '../../../../src/data/topbar-text.json';
 
 function HeaderShop({ options }) {
 
     return (
         <Fragment>
-            {/* Start header */}
+            {/* start header */}
             <header id="header" className="site-header header-style-1">
                 <div className="topbar">
                     <div className="topbar-text">
+                        {/* load data from static content */}
                         <p>{data.content}</p>
                     </div>
                 </div>
@@ -21,19 +21,19 @@ function HeaderShop({ options }) {
                 <nav className="navigation navbar navbar-default">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <button type="button" className="open-btn" onClick={options.onMobleNavClick}>
+                            {/* toogle navigation for mobile devices */}
+                            <button type="button" className="open-btn" onClick={options.onMobleNavClick}> 
                                 <span className="sr-only">Toggle navigation</span>
                                 <span className="icon-bar"/>
                                 <span className="icon-bar"/>
                                 <span className="icon-bar"/>
                             </button>
                             <Link className="mobile-only navbar-brand" to="/">
-                                <img src={process.env.PUBLIC_URL + "/assets/images/logo.png"} alt=""/>
+                                {/* dispaying logo */}
+                                <img src={process.env.PUBLIC_URL + "logo64px.png"} alt="logo"/> 
                             </Link>
 
                         </div>
-
-                        <HeaderLeft options={options}/>
 
                         <Navbar options={options}/>
 
