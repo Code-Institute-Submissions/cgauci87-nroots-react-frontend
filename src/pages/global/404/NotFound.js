52c4aa06b1aae663afabed01c8e508c3e917a245
cgauci87 <cgauci@gmail.com>
2023-01-28 18:02:31 +0000

    added 404 Page (Not Found) -- src/pages/global/404/NotFound.js

--- src/pages/global/404/NotFound.js
+++ src/pages/global/404/NotFound.js
@@ -1,5 +1,5 @@
-import React, {Fragment} from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
 // import components
 import HeaderShop from "../../../components/shop/navbar/HeaderShop";
 import Footer from "../../../components/shop/footer/Footer";
@@ -7,33 +7,35 @@ import Footer from "../../../components/shop/footer/Footer";
 //  404 Page (Not Found)
 
 function NotFound({ options }) {
+  return (
+    <Fragment>
+      {/* HeaderShop with options */}
+      <HeaderShop options={options} />
 
-    return (
-        <Fragment>
-            {/* HeaderShop with options */}
-            <HeaderShop options={options} />
-
-            {/* start error-404-section */}
-            <section className="error-404-pg error-404-section section-padding">
-                <div className="container-1410">
-                    <div className="error-404-area">
-                        <h2>404</h2>
-                        <div className="error-message">
-                            <h3>Oops! Page Not Found!</h3>
-                            <p>We’re sorry but we can’t seem to find the page you requested. This might be because you
-                                have
-                                typed the web address incorrectly.</p>
-                            <NavLink to="/" className="theme-btn">Back to Home</NavLink>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* end error-404-section */}
-            {/* Footer component  */}
-            <Footer/>
-
-        </Fragment>
-    );
+      {/* start error-404-section */}
+      <section className="error-404-pg error-404-section section-padding">
+        <div className="container-1410">
+          <div className="error-404-area">
+            <h2>404</h2>
+            <div className="error-message">
+              <h3>Oops! Page Not Found!</h3>
+              <p>
+                We’re sorry but we can’t seem to find the page you requested.
+                This might be because you have typed the web address
+                incorrectly.
+              </p>
+              <NavLink to="/" className="theme-btn">
+                Back to Home
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* end error-404-section */}
+      {/* Footer component  */}
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default NotFound;
