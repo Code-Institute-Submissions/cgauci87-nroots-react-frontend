b7046a1552995813565b5a86165ea1d89cc4f20c
cgauci87 <cgauci@gmail.com>
2023-01-28 16:48:04 +0000

    added Categories Component in 'shop' -- src/components/shop/products/Categories.js

---
+++ src/components/shop/products/Categories.js
@@ -0,0 +1,48 @@
+import React, {Fragment} from 'react';
+
+//  import Categories data content for icons
+import categoriesData from '../../../data/categories.json';
+import {NavLink} from "react-router-dom";
+
+function Categories() {
+
+    return (
+        <Fragment>
+            {/* start category-section-area */}
+            <section className="category-section-area section-padding">
+                <div className="container-1410">
+                    <div className="row">
+                        <div className="col col-xs-12">
+                            <div className="section-title-s3">
+                                <h2>nRoots Categories</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col col-xs-12">
+                            <div className="all-cat">
+                                <ul className="clearfix">
+                                    { // load icons and name from data
+                                        categoriesData.map((item, index) => (
+                                            <li key={index}>
+                                                <NavLink to={item.link}>
+                                                    <i className={"fi " + item.icon}/>
+                                                    <span>{item.name}</span>
+                                                </NavLink>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* end container-1410 */}
+            </section>
+            {/* end category-section-area */}
+        </Fragment>
+    );
+}
+
+
+export default Categories;
