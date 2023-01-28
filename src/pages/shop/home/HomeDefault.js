b8d26ef4bcb8a7529ac5243585f91368f1ba9b47
cgauci87 <cgauci@gmail.com>
2023-01-28 17:20:58 +0000

    added HomeDefault page -- src/pages/shop/home/HomeDefault.js

--- src/pages/shop/home/HomeDefault.js
+++ src/pages/shop/home/HomeDefault.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Fragment } from "react";
 
-import Header from "../../components/header/HeaderShop";
-import Hero1 from "../../components/Hero/Hero1";
-import FeaturedProducts from "../../components/products/FeaturedProducts";
-import Categories from "../../components/categories/Categories";
-import Footer from "../../components/global/Footer";
+// import components
+import HeaderShop from "../../../components/shop/navbar/HeaderShop";
+import Hero1 from "../../../components/shop/hero/Hero1";
+import FeaturedProducts from "../../../components/shop/products/FeaturedProducts";
+import Categories from "../../../components/shop/products/Categories";
+import Footer from "../../../components/shop/footer/Footer";
 
-// HomeDefault page
+// HomeDefault Page
 function HomeDefault({ options }) {
   return (
     <Fragment>
-      <Header options={options} />
-
+      {/* HeaderShop component with options props*/}
+      <HeaderShop options={options} />
+      {/* Hero1 component */}
       <Hero1 />
-
+      {/* FeaturedProducts component */}
       <FeaturedProducts />
-
+      {/* Categories component */}
       <Categories />
-
+      {/* Footer Component */}
       <Footer />
     </Fragment>
   );
