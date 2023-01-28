3ec473e5f498eaeaf6445c12832e6afbf1c850a5
cgauci87 <cgauci@gmail.com>
2023-01-28 18:29:23 +0000

    added PageTitle component -- src/components/shop/pageTitle/PageTitle.js

---
+++ src/components/shop/pageTitle/PageTitle.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
+// PageTitle Component
+
+function PageTitle({ name }) {
+  return (
+    <Fragment>
+      {/* start page-title */}
+      <section className="page-title">
+        <div className="page-title-container">
+          <div className="page-title-wrapper">
+            <div className="container">
+              <div className="row">
+                <div className="col col-xs-12">
+                  <h2>{name}</h2>
+                  {/* display breadcrumb */}
+                  <ol className="breadcrumb">
+                    <li>
+                      <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>{name}</li>
+                  </ol>
+                </div>
+              </div>
+              {/* end row */}
+            </div>
+            {/* end container */}
+          </div>
+        </div>
+      </section>
+      {/* end page-title */}
+    </Fragment>
+  );
+}
+
+export default PageTitle;
