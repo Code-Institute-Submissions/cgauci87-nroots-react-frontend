12f4ce37758e0132790398b17b0d5ce49643ab31
cgauci87 <cgauci@gmail.com>
2023-01-29 08:18:37 +0000

    added axiosDefaults -- src/api/axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// let baseUrl = "https://8000-cgauci87-mycodrfapi-0c7dgbgbron.ws-eu84.gitpod.io/";
-let baseUrl = "https://myco-malta.herokuapp.com/";
+let baseUrl = "https://8000-cgauci87-nrootsdrfapi-j8v838srezm.ws-eu84.gitpod.io/"; // Development
 axios.defaults.baseURL = baseUrl;
 axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -45,14 +44,3 @@ export const authPrivateAxios = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// export const authAxios = axios.create({
-//   baseURL: baseUrl,
-//   headers: { "content-type": "application/json" },
-// });
-
-// authAxios.interceptors.request.use((config) => {
-//   let accessToken = localStorage.getItem("access_token");
-//   config.headers.Authorization = `Bearer ${accessToken}`;
-//   return config;
-// });
