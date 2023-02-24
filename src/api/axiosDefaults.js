3b41be1d169777fd13643e8c40aa4ee397e833dc
cgauci87 <cgauci@gmail.com>
2023-02-24 09:20:47 +0000

    update eslint

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 
-let baseUrl = "https://nroots-drf-api.herokuapp.com/"; // DRF API PRODUCTION
-// let baseUrl = "https://8080-cgauci87-nrootsdrfapi-6m4oduklif1.ws-eu87.gitpod.io/";
+// let baseUrl = "https://nroots-drf-api.herokuapp.com/"; // DRF API PRODUCTION
+let baseUrl = "https://8080-cgauci87-nrootsdrfapi-6m4oduklif1.ws-eu87.gitpod.io/";
 axios.defaults.baseURL = baseUrl;
 axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = "csrftoken";
